69fb791c4b50a6e37d82a5d93645c433948022fb
PyCaret <moez@pycaret.org>
2020-07-12 11:38:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import streamlit as st
 
 def main():
     from pycaret.utils import version
-    ver = version()
-    st.sidebar.success(ver + ' succesfully loaded.')
+    msg = str(version()) + ' succesfully loaded.' 
+    st.success(msg)
 
 if __name__ == '__main__':
     main()
